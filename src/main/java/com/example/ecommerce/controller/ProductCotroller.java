c5c61cd6f888d01dd6868d0fc0cd078f431f3766
paola <paomedina_88@hotmail.com>
2021-08-27 00:26:05 -0300

    conectado con fetch

--- src/main/java/com/example/ecommerce/controller/ProductCotroller.java
+++ src/main/java/com/example/ecommerce/controller/ProductCotroller.java
@@ -9,6 +9,7 @@ import com.example.ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class ProductCotroller {
     @Autowired
